b28dbc7b385d3017a80186f89dbfc0c01d1ec1d9
VjSuxo <vjoelsuxod@gmail.com>
2024-06-14 18:48:05 -0400

    base

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
+import ModalC from './components/ModalC'; // Corrected import statement
+import CardC from './components/CardC';
 
 function App() {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +17,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button color="danger" onClick={toggle}>
+          Open Modal
+        </Button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +28,10 @@ function App() {
         >
           Learn React
         </a>
+        <ModalC modal={modal} toggle={toggle} /> {/* Pass modal and toggle props */}
+        <CardC/>
+        <CardC/>
+        <CardC/>
       </header>
     </div>
   );
